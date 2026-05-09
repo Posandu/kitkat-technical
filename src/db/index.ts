d46bf93b4870f801124e6365217320026e6978f6
Posandu <posandumapa@gmail.com>
2026-05-09 19:31:21 +0530

    concurrent shi

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,4 +4,9 @@ import * as schema from "./schema";
 
 const sqlite = new Database("sqlite.db");
 
+// Enable WAL mode for better concurrency
+sqlite.exec("PRAGMA journal_mode = WAL;");
+// Set busy timeout to 5 seconds
+sqlite.exec("PRAGMA busy_timeout = 5000;");
+
 export const db = drizzle({ client: sqlite, schema });
